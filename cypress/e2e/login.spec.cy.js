e63fe37a2eb22dbe0c31a8ec885f6c0ab20995bd
Fernanda Bess <fernanda@sabios.com.br>
2025-11-13 16:12:19 -0300

    Base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -10,22 +10,15 @@ describe("Orange HRM tests", () => {
   };
 
   it("Login - success", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    );
+    cy.visit("/auth/login");
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username);
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password);
     cy.get(selectorsList.submitButton).click();
-    cy.url().should(
-      "eq",
-      "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
-    );
+    cy.location("pathname").should("eq", "/web/index.php/dashboard/index");
     cy.get(selectorsList.dashboardGrid);
   });
   it("Login - failed", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    );
+    cy.visit("/auth/login");
     cy.get(selectorsList.usernameField).type(userData.userFailed.username);
     cy.get(selectorsList.passwordField).type(userData.userFailed.password);
     cy.get(selectorsList.submitButton).click();
